d57e54eab1295d0872b1db0a1dc5746e8c119bc4
marcelinho21junior <marcelo.belitardo@fatec.sp.gov.br>
2022-03-07 17:13:25 -0300

    COMMIT APP OK

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
+import { useState } from 'react';
 import {FiSearch} from 'react-icons/fi'
+import api from './services/api';
 import './styles.css'
 
 function App() {
+
+  const [input, setInput] = useState('');
+  const [cep, setCep] =useState({});
+
+  async function handleSearch(){
+    
+    if(input ===''){
+      alert("Preencha um CEP!")
+      return
+    }
+
+    if(input.length != 8){
+      alert("O CEP deverá ter 8 números!!")
+    }else{
+      alert(input)/*tirar este*/
+    }
+
+    try{
+      const response = await api.get(`${input}/json`)
+      setCep(response.data)
+      console.log(response.data)
+      setInput("")
+    }catch{
+      alert("Ops erro ao buscar! ")
+      setInput("")
+    }
+  }
+
   return (
     <div className="container">
       <h1 className='title'>Buscador de CEP</h1>
@@ -9,23 +39,26 @@ function App() {
       <div className='containerInput'>
         <input
           type="text"
-          placeholder='Digite seu cep...'
+          placeholder='Digite seu CEP...'
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
         />
 
-        <button className='buttonSearch'>  
+        <button className='buttonSearch' onClick={handleSearch}>  
           <FiSearch size={25} color="#FFF"/>
         </button>
       </div>
 
-      <main className='main'>
-        <h2>CEP: 79003222</h2>
+      {Object.keys(cep).length > 0 && (
+        <main className='main'>
+          <h2>CEP: {cep.cep}</h2>
 
-        <span>rUA TESTE ALGUM</span>
-        <span>Complemento: alguma</span>
-        <span>vila rosa</span>
-        <span>Campo grande - ms</span>
-      </main>
-
+          <span>logradouro: {cep.logradouro}</span>
+          <span>Complemento: {cep.complemento}</span>
+          <span>Bairro:{cep.bairro}</span>
+          <span>{cep.localidade} - {cep.uf}</span>
+        </main>
+      )}
     </div>
   );
 }
